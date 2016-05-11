3629e46b61a51dc5165fbca096a6b67db1e5b09c
Nery <neryortez@gmail.com>
2016-05-10 18:05:09 -0600

    Mucho por hacer
    mucho..!

--- app/src/main/java/com/example/efaa/iee/PlaceHolderFragment.java
+++ app/src/main/java/com/example/efaa/iee/PlaceHolderFragment.java
@@ -5,24 +5,23 @@ package com.example.efaa.iee;
  */
 
 import android.app.LoaderManager;
-import android.content.Context;
-import android.content.CursorLoader;
+import android.content.AsyncTaskLoader;
 import android.content.Loader;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.CheckBox;
-import android.widget.CursorAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,7 +29,7 @@ import java.util.ArrayList;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class PlaceholderFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class PlaceholderFragment extends Fragment implements LoaderManager.LoaderCallbacks<ArrayList> {
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -107,6 +106,15 @@ public class PlaceholderFragment extends Fragment implements LoaderManager.Loade
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Create a progress bar to display while the list loads
+        ProgressBar progressBar = new ProgressBar(getContext());
+        progressBar.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        progressBar.setIndeterminate(true);
+        recyclerView.addView(progressBar);
+
+
         //Inflar la pestaña con el fragmento que contiene el ListView o el RecyclerView
 //        View rootView = inflater.inflate(R.layout.fragment_tab, container, false);
         View rootView = inflater.inflate(R.layout.reciclador_tab, container, false);
@@ -167,64 +175,37 @@ public class PlaceholderFragment extends Fragment implements LoaderManager.Loade
         //Seteamos esos escuchas
 //        lista.setOnItemClickListener(listener);
         recyclerView.setOnClickListener(escucha);
+        // Prepare the loader.  Either re-connect with an existing one,
+        // or start a new one.
+        Bundle bundle1 = new Bundle();
+        bundle1.putString("COLUMNA", columna);
+        getLoaderManager().initLoader(0, bundle1,
+                (android.support.v4.app.LoaderManager.LoaderCallbacks<Object>) getContext());
         return rootView;
     }
 
 
-    /**
-     * Instantiate and return a new Loader for the given ID.
-     *
-     * @param id   The ID whose loader is to be created.
-     * @param args Any arguments supplied by the caller.
-     * @return Return a new Loader instance that is ready to start loading.
-     */
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(this, )
+
+    // Called when a new Loader needs to be created
+    public Loader<ArrayList> onCreateLoader(int id, Bundle args) {
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        final String columna = args.getString("COLUMNA");
+
+        return new AsyncTaskLoader<ArrayList>(getContext()) {
+            @Override
+            public ArrayList<Clase> loadInBackground() {
+                ArrayList array = new ArrayList();
+                array = dataSource.queryPasadasODisponibles(dob, COLUMNA, "1", getContext());
+                return array;
+            }
+        };
     }
 
-    /**
-     * Called when a previously created loader has finished its load.  Note
-     * that normally an application is <em>not</em> allowed to commit fragment
-     * transactions while in this call, since it can happen after an
-     * activity's state is saved.  See {@link FragmentManager#beginTransaction()
-     * FragmentManager.openTransaction()} for further discussion on this.
-     * <p/>
-     * <p>This function is guaranteed to be called prior to the release of
-     * the last data that was supplied for this Loader.  At this point
-     * you should remove all use of the old data (since it will be released
-     * soon), but should not do your own release of the data since its Loader
-     * owns it and will take care of that.  The Loader will take care of
-     * management of its data so you don't have to.  In particular:
-     * <p/>
-     * <ul>
-     * <li> <p>The Loader will monitor for changes to the data, and report
-     * them to you through new calls here.  You should not monitor the
-     * data yourself.  For example, if the data is a {@link Cursor}
-     * and you place it in a {@link CursorAdapter}, use
-     * the {@link CursorAdapter#CursorAdapter(Context,
-     * Cursor, int)} constructor <em>without</em> passing
-     * in either {@link CursorAdapter#FLAG_AUTO_REQUERY}
-     * or {@link CursorAdapter#FLAG_REGISTER_CONTENT_OBSERVER}
-     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
-     * from doing its own observing of the Cursor, which is not needed since
-     * when a change happens you will get a new Cursor throw another call
-     * here.
-     * <li> The Loader will release the data once it knows the application
-     * is no longer using it.  For example, if the data is
-     * a {@link Cursor} from a {@link CursorLoader},
-     * you should not call close() on it yourself.  If the Cursor is being placed in a
-     * {@link CursorAdapter}, you should use the
-     * {@link CursorAdapter#swapCursor(Cursor)}
-     * method so that the old Cursor is not closed.
-     * </ul>
-     *
-     * @param loader The Loader that has finished.
-     * @param data   The data generated by the Loader.
-     */
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+    public void onLoadFinished(Loader<ArrayList> loader, ArrayList data) {
+        cAdaptador = new ClaseRecyclerAdaptador(data);
+        recyclerView.swapAdapter(cAdaptador, true);
     }
 
     /**
@@ -235,8 +216,9 @@ public class PlaceholderFragment extends Fragment implements LoaderManager.Loade
      * @param loader The Loader that is being reset.
      */
     @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
+    public void onLoaderReset(Loader<ArrayList> loader) {
 
     }
+
 }
 
